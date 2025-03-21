64027922c7be185171127b57c83f2d4073f1181c
ZatchBell <141612373+Piyush79432@users.noreply.github.com>
2025-03-21 09:12:28 +0530

    Update truffle-config.js

--- src/truffle-config.js
+++ src/truffle-config.js
@@ -6,7 +6,7 @@ module.exports = {
       provider: () =>
         new HDWalletProvider(
           "42a8078df4f1fe49e1ba13f3629ea4fe4c12e0a931dd3640dcfd58f7fb33e4a7", // ⚠ Replace ASAP
-          "https://eth-sepolia.g.alchemy.com/v2/BdczhLlBwIFSdqt_sdAaQj2nb9hEAEOC"
+          "https://eth-sepolia.g.alchemy.com/v2/Your_Alchemy_API_KEY"
         ),
       network_id: 11155111, // Sepolia network ID
       gas: 5000000,
